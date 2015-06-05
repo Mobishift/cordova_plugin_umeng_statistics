eeb18d71e52fbb8d99cf273cd6313b85c585cae0
Zhengz <zhengz@masn.cn>
2015-06-05 11:06:35 +0800

    初始化

---
+++ src/android/UMengStatistics.java
@@ -0,0 +1,33 @@
+package com.mobishift.umengstatistics;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.umeng.analytics.MobclickAgent;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class UMengStatistics extends CordovaPlugin {
+    private static final String PAGE_START = "pageStart";
+    private static final String PAGE_END = "pageEnd";
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if(action.equals(PAGE_START)){
+            String pageName = args.getString(0);
+            MobclickAgent.onPageStart(pageName);
+            return true;
+        }else if(action.equals(PAGE_END)){
+            String pageName = args.getString(0);
+            MobclickAgent.onPageEnd(pageName);
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ www/UMengStatistics.js
@@ -0,0 +1,14 @@
+var exec = require('cordova/exec');
+
+var noop = function(){};
+exports.exec = function(action, arg0, success, error) {
+    exec(success, error, "UMengStatistics", action, [arg0]);
+};
+
+exports.pageStart = function(pageName){
+	exports.exec('pageStart', pageName, noop, noop);
+};
+
+exports.pageEnd = function(pageName){
+	exports.exec('pageEnd', pageName, noop, noop);
+};
